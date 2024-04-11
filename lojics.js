3c291d46ed51dca2852a89a0900e0e93ba088f6a
LRLayan <rameshlayan4@gmail.com>
2024-04-12 02:54:17 +0530

    scroll animation logic

---
+++ lojics.js
@@ -0,0 +1,13 @@
+
+    const obServer = new IntersectionObserver((entries) =>{
+        entries.forEach((entry) =>{
+            if (entry.isIntersecting){
+                entry.target.classList.add("show");
+            }else {
+                entry.target.classList.remove("show")
+            }
+        })
+    })
+
+    const  hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => obServer.observe(el))
